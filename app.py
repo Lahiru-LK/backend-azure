7501d4ad59d34f65837cef94a27d321b52272a9d
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-05-26 14:33:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'message': 'Hello from Flask on Azure! LK'})
+    return jsonify({'message': 'Hello from Flask on Azure! xxxxxxxx'})
 
 if __name__ == '__main__':
     app.run()
